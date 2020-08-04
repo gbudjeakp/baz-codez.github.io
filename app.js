e6d46e8402816dd234bda81dd2065a120b3c5180
Akpevwen Gbudje <gbudjeakp@gmail.com>
2020-08-03 23:26:11 -0500

    fixes

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ function contact () {
 }
 
 // for nav menu
-var menu = document.querySelector('.burger')
-var sideNav = document.getElementById('sideNav')
-sideNav.style.right = '-250px'
-menu.addEventListener('click', popOut)
-function popOut () {
-  if (sideNav.style.right === '-250px') {
-    sideNav.style.right = '0'
-  } else {
-    sideNav.style.right = '-250px'
-  }
-}
+// var menu = document.querySelector('.burger')
+// var sideNav = document.getElementById('sideNav')
+// sideNav.style.right = '-250px'
+// menu.addEventListener('click', popOut)
+// function popOut () {
+//   if (sideNav.style.right === '-250px') {
+//     sideNav.style.right = '0'
+//   } else {
+//     sideNav.style.right = '-250px'
+//   }
+// }
