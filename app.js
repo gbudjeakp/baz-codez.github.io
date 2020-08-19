2ad86cc665b8b0d398ad1738f601a348a3801577
Akpevwen Gbudje <gbudjeakp@gmail.com>
2020-08-18 22:48:03 -0500

    updating

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-particlesJS.load('particles-js', 'particles.json')
+particlesJS.load('particles-js', 'particles.json');
 
 // For Contact Button
-const btn = document.querySelector('.btn')
-btn.addEventListener('click', contact)
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', contact);
 
 function contact () {
-  window.location.href = 'mailto:gbudjeakp@gmail.com'
+  window.location.href = 'mailto:gbudjeakp@gmail.com';
 }
+
 
 // for nav menu
 // var menu = document.querySelector('.burger')
@@ -20,3 +21,38 @@ function contact () {
 //     sideNav.style.right = '-250px'
 //   }
 // }
+
+//for popup
+const popup = document.getElementsByClassName('popup')
+for (var i = 0 ; i < popup.length; i++) {
+  popup[i].addEventListener('onmouseover' , pops , false ) ; 
+}
+
+function pops(){
+  
+  const popup = document.getElementsByClassName('popup')
+for (var i = 0 ; i < popup.length; i++) {
+  popup[i].addEventListener('onmouseover' , pops , false ) ; 
+}
+
+const loader = document.getElementsByClassName('loader');
+for(var i =0; i<loader.length; i++){
+loader[i].style.display = 'block';
+ }
+}
+
+// mouse out closes popup
+function popoff(){
+    
+  const popup = document.getElementsByClassName('popup')
+for (var i = 0 ; i < popup.length; i++) {
+  popup[i].addEventListener('onmouseover' , pops , false ) ; 
+}
+
+
+const loader = document.getElementsByClassName('loader');
+for(var i =0; i<loader.length; i++){
+  loader[i].style.display = 'none';
+ 
+   }
+}
