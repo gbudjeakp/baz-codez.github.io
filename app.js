05b2415baa2f66cf3f4ee0252d20435a3907841e
Akpevwen Gbudje <gbudjeakp@gmail.com>
2020-10-08 21:01:19 -0500

    update

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-particlesJS.load('particles-js', 'particles.json');
+particlesJS.load('particles-js', 'particles.json')
 
 // For Contact Button
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', contact);
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', contact)
 
 function contact () {
-  window.location.href = 'mailto:gbudjeakp@gmail.com';
+  window.location.href = 'mailto:gbudjeakp@gmail.com'
 }
-
 
 // for nav menu
 // var menu = document.querySelector('.burger')
@@ -22,37 +21,34 @@ function contact () {
 //   }
 // }
 
-//for popup
+// for popup
 const popup = document.getElementsByClassName('popup')
-for (var i = 0 ; i < popup.length; i++) {
-  popup[i].addEventListener('onmouseover' , pops , false ) ; 
+for (var i = 0; i < popup.length; i++) {
+  popup[i].addEventListener('onmouseover', pops, false)  
 }
 
-function pops(){
-  
+function pops () {
   const popup = document.getElementsByClassName('popup')
-for (var i = 0 ; i < popup.length; i++) {
-  popup[i].addEventListener('onmouseover' , pops , false ) ; 
+  for (var i = 0; i < popup.length; i++) {
+    popup[i].addEventListener('onmouseover', pops, false)  
 }
 
-const loader = document.getElementsByClassName('loader');
-for(var i =0; i<loader.length; i++){
-loader[i].style.display = 'block';
+  const loader = document.getElementsByClassName('loader')
+for (var i = 0; i < loader.length; i++) {
+    loader[i].style.display = 'block'
  }
 }
 
 // mouse out closes popup
-function popoff(){
-    
+function popoff () {
   const popup = document.getElementsByClassName('popup')
-for (var i = 0 ; i < popup.length; i++) {
-  popup[i].addEventListener('onmouseover' , pops , false ) ; 
+  for (var i = 0; i < popup.length; i++) {
+    popup[i].addEventListener('onmouseover', pops, false)  
 }
 
-
-const loader = document.getElementsByClassName('loader');
-for(var i =0; i<loader.length; i++){
-  loader[i].style.display = 'none';
+  const loader = document.getElementsByClassName('loader')
+for (var i = 0; i < loader.length; i++) {
+    loader[i].style.display = 'none'
  
    }
 }
