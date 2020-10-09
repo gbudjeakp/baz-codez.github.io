75ac00f7dddff98474944cc82edc969154382a1f
Akpevwen Gbudje <gbudjeakp@gmail.com>
2020-10-08 21:11:55 -0500

    update

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-particlesJS.load('particles-js', 'particles.json')
 
 // For Contact Button
-const btn = document.querySelector('.btn')
-btn.addEventListener('click', contact)
+// const btn = document.querySelector('.btn')
+// btn.addEventListener('click', contact)
 
-function contact () {
-  window.location.href = 'mailto:gbudjeakp@gmail.com'
-}
+// function contact () {
+//   window.location.href = 'mailto:gbudjeakp@gmail.com'
+// }
 
 // for nav menu
 // var menu = document.querySelector('.burger')
@@ -22,33 +21,31 @@ function contact () {
 // }
 
 // for popup
-const popup = document.getElementsByClassName('popup')
-for (var i = 0; i < popup.length; i++) {
-  popup[i].addEventListener('onmouseover', pops, false)  
-}
 
-function pops () {
-  const popup = document.getElementsByClassName('popup')
-  for (var i = 0; i < popup.length; i++) {
-    popup[i].addEventListener('onmouseover', pops, false)  
-}
+// Set the date we're counting down to
+var countDownDate = new Date('Oct 8, 2021 15:37:25').getTime()
 
-  const loader = document.getElementsByClassName('loader')
-for (var i = 0; i < loader.length; i++) {
-    loader[i].style.display = 'block'
- }
-}
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get todays date and time
+  var now = new Date().getTime()
 
-// mouse out closes popup
-function popoff () {
-  const popup = document.getElementsByClassName('popup')
-  for (var i = 0; i < popup.length; i++) {
-    popup[i].addEventListener('onmouseover', pops, false)  
-}
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now
 
-  const loader = document.getElementsByClassName('loader')
-for (var i = 0; i < loader.length; i++) {
-    loader[i].style.display = 'none'
- 
-   }
-}
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+  // Display the result in an element with id="demo"
+  document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h ' +
+  minutes + 'm ' + seconds + 's ';
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x)
+    document.getElementById('demo').innerHTML = 'EXPIRED';
+  }
+}, 1000)
