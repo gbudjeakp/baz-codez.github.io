6e00d98207afd19ae2ef19756247e3de850d8144
Akpevwen Gbudje <gbudjeakp@gmail.com>
2020-12-02 12:22:09 -0600

    finished

--- app.js
+++ app.js
@@ -15,20 +15,7 @@ $(document).on('scroll', function () {
   }
 })
 
-// for cursor
-const cursor = document.querySelector('.cursor')
 
-document.addEventListener('mousemove', e => {
-  cursor.setAttribute('style', 'top: ' + (e.pageY - 15) + 'px; left: ' + (e.pageX - 15) + 'px;')
-})
-
-document.addEventListener('click', () => {
-  cursor.classList.add('expand')
-
-  setTimeout(() => {
-    cursor.classList.remove('expand')
-  }, 500)
-})
 
 // For dynamic date
 const text = document.getElementById('footer')
