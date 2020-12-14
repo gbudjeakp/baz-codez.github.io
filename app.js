f3558622b14865ddc7b05bfbbf8cb6388c362c46
Akpevwen Gbudje <gbudjeakp@gmail.com>
2020-12-14 14:44:09 -0600

    responsive fix

--- app.js
+++ app.js
@@ -15,12 +15,7 @@ $(document).on('scroll', function () {
   }
 })
 
-const mainNav = document.getElementById('js-menu')
-const navBarToggle = document.getElementById('js-navbar-toggle')
 
-navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active')
-})
 
 // For dynamic date
 const text = document.getElementById('footer')
